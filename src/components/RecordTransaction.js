2f28edd9015d09a87ee414fba479a6c2b668bf63
nadimakk <nadimakkaoui1@gmail.com>
2023-04-19 12:45:58 +0400

    complete Record an Exchange component

--- src/components/RecordTransaction.js
+++ src/components/RecordTransaction.js
@@ -1,18 +1,60 @@
 import React, {useEffect, useState} from 'react';
-import { TextField, Typography } from '@mui/material';
 import "../css/recordTransaction.css"
 import usaFlag from "../media/usaFlag.png";
 import lebanonFlag from "../media/lebanonFlag.png";
 import {transactionType} from "../enums/transactionType";
+import {
+    Button,
+    TextField,
+    Typography,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    Alert,
+    Snackbar } from "@mui/material";
 
 
-function RecordTransaction() {
-    let [usdAmount, setUsdAmount] = useState(null);
-    let [lbpAmount, setLbpAmount] = useState(null);
+function RecordTransaction({handleRecordTransactionSubmit}) {
+    let [usdAmount, setUsdAmount] = useState('');
+    let [lbpAmount, setLbpAmount] = useState('');
+    let [exchangeType, setExchangeType] = useState('');
+    const [missingInput, setMissingInput] = useState(false);
+
+    function handleUsdAmountChange(event) { setUsdAmount(event.target.value); }
+    function handleLbpAmountChange(event) { setLbpAmount(event.target.value); }
+    function handleExchangeTypeChange(event) { setExchangeType(event.target.value); }
+
+    function handleSubmitButtonClick() {
+        if (!usdAmount || !lbpAmount || !exchangeType) {
+            setMissingInput(true)
+            return;
+        }
+        handleRecordTransactionSubmit(usdAmount, lbpAmount, exchangeType);
+    }
+
+    function handleClearButtonClick() {
+        setUsdAmount("");
+        setLbpAmount("");
+        setExchangeType("");
+    }
+
+    function closeAlert() {
+        setMissingInput(false);
+    };
 
 
     return (
     <>
+        <Snackbar
+            open={missingInput}
+            autoHideDuration={3000}
+            onClose={closeAlert}
+            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        >
+            <Alert severity="error">Please fill in all inputs before submitting the form.</Alert>
+        </Snackbar>
+
         <div className="currency-row">
         <div>
             <Typography variant="subtitle1" align="center" className="currency-label">
@@ -25,8 +67,9 @@ function RecordTransaction() {
                 variant="outlined"
                 className="amount-textfield"
                 fullWidth
-                placeholder="Enter amount of USD in transaction"
+                label="Enter amount of USD in exchange"
                 value={usdAmount}
+                onChange={handleUsdAmountChange}
             />
         </div>
     </div>
@@ -43,11 +86,48 @@ function RecordTransaction() {
                     variant="outlined"
                     className="amount-textfield"
                     fullWidth
-                    placeholder="Enter amount of LBP in transaction"
+                    label="Enter amount of LBP in exchange"
                     value={lbpAmount}
+                    onChange={handleLbpAmountChange}
                 />
             </div>
         </div>
+        <div className="menu-row">
+            <FormControl sx={{ m: 1, minWidth: 160 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">
+                    Exchange Type
+                </InputLabel>
+                <Select
+                    label="Exchange Type"
+                    value={exchangeType}
+                    onChange={handleExchangeTypeChange}
+                    autoWidth
+                >
+                    <MenuItem value={transactionType.UsdToLbp}>USD to LBP</MenuItem>
+                    <MenuItem value={transactionType.LbpToUsd}>LBP to USD</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
+        <div className="button-row">
+            <Button
+                className="record-transaction-button"
+                variant="contained"
+                size="large"
+                onClick={handleSubmitButtonClick}
+            >
+                Submit
+            </Button>
+            <Button
+                className="record-transaction-button"
+                variant="contained"
+                size="large"
+                onClick={handleClearButtonClick}
+            >
+                Clear
+            </Button>
+        </div>
+
+
     </>
     );
 }
--- src/pages/TransactionService.js
+++ src/pages/TransactionService.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, {useState} from 'react';
 import "../css/transactionService.css"
 import RecordTransaction from "../components/RecordTransaction.js"
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { transactionType } from "../enums/transactionType.js";
 
 function TransactionService() {
+    let [usdAmountRecordTransaction, setUsdAmountRecordTransaction] = useState(null);
+    let [lbpAmountRecordTransaction, setLbpAmountRecordTransaction] = useState(null);
+    let [exchangeTypeRecordTransaction, setExchangeTypeRecordTransaction] = useState(null);
+
+
+    //to make an API call in this file when a button is clicked in the RecordTransaction component,
+    //I learned that I can define a function here and pass it as a prop to the component, and call the function when the button
+    //is clicked. I used these links as a source:
+    //https://stackoverflow.com/questions/48367998/passing-event-and-props-from-child-to-parent-in-react
+    //https://stackoverflow.com/questions/60915262/how-to-pass-function-as-props-from-functional-parent-component-to-child
+    function handleRecordTransactionSubmit(usdAmount, lbpAmount, exchangeType) {
+        setUsdAmountRecordTransaction(usdAmount);
+        setLbpAmountRecordTransaction(lbpAmount);
+        setExchangeTypeRecordTransaction(exchangeType);
+    }
+
+
     return (
         //source: https://dev.to/dawnind/3-ways-to-display-two-divs-side-by-side-3d8b#:~:text=The%20most%20common%20way%20to,using%20inline%2Dblock%20css%20property.&text=The%20inline%2Dblock%20property%20on,like%20an%20inline%20element%20does
         //I used the above source to find an organized way to place 2 cards side by side and taking up the whole page
+
+        //source: https://mui.com/material-ui/react-select/
+        //I used the above source to design the drop down menu
         <div className="flex-parent-element">
             <div className="flex-child-element">
                 <div className="header">
-                    <h1>Record a Transaction</h1>
+                    <h1>Record an Exchange</h1>
                 </div>
                 <div className="flex-child-body">
-                    <RecordTransaction/>
+                    <p className="record-transaction-instructions">
+                        To record an exchange you made, please input the amounts of each currency that were
+                        exchanged in your transaction. Then, select the type of the exchange.
+                    </p>
+                    <RecordTransaction onSubmit={handleRecordTransactionSubmit}/>
+
                 </div>
             </div>
             <div className="flex-child-element">
