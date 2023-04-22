0669d76e75d8c74bd38a2f1c58c098557741f5e4
nadimakk <nadimakkaoui1@gmail.com>
2023-04-22 21:45:40 +0300

    add alerts to Pending Offers page, design improvements, automatic refresh

--- src/pages/PendingOffers.js
+++ src/pages/PendingOffers.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
-import "../css/myExchangeHistory.css";
+import "../css/pendingOffers.css";
 import { baseUrl } from "../config/Config.js";
 import { getUserToken } from "../utility/tokenStorage";
 import UserOffersTable from "../components/UserOffersTable";
+import SnackbarAlert from "../components/SnackbarAlert";
 
 function PendingOffers() {
   let [receivedOffers, setReceivedOffers] = useState();
   let [sentOffers, setSentOffers] = useState();
+  let [acceptOfferSuccess, setAcceptOfferSuccess] = useState(false);
+  let [declineOfferSuccess, setDeclineOfferSuccess] = useState(false);
 
   useEffect(() => {
     fetchIncomingOffers();
@@ -60,35 +63,59 @@ function PendingOffers() {
     try {
       const response = await fetch(`${baseUrl}/offer/process-offer`, requestOptions);
       const data = await response.json();
-      return data;
+      if (accepted) {
+        setAcceptOfferSuccess(true);
+      } else {
+        setDeclineOfferSuccess(true);
+      }
+      fetchIncomingOffers();
     } catch (error) {
       console.error("Error accepting offer:", error);
       throw error;
     }
   }
 
-  return (
-    <div className="wrapper">
-      <div className="header">
-        <h1>Pending Offers</h1>
-      </div>
+  function closeSuccessAlert() {
+    setAcceptOfferSuccess(false);
+    setDeclineOfferSuccess(false);
+  }
 
-      <div className="wrapper-content">
-        <h2 className="section-title">Incoming Offers</h2>
-        {/* Must wait for transactionsData before rendering component
+  return (
+    <>
+      <SnackbarAlert
+        open={acceptOfferSuccess}
+        message="Successfully accepted the offer."
+        onClose={closeSuccessAlert}
+        severity="success"
+      />
+      <SnackbarAlert
+        open={declineOfferSuccess}
+        message="Successfully declined the offer."
+        onClose={closeSuccessAlert}
+        severity="success"
+      />
+      <div className="wrapper pending-offers-container">
+        <div className="header">
+          <h1>Pending Offers</h1>
+        </div>
+
+        <div className="wrapper-content">
+          <h2 className="section-title">Incoming Offers</h2>
+          {/* Must wait for transactionsData before rendering component
         source: https://stackoverflow.com/questions/74656680/react-how-to-wait-on-prop-data-before-rendering-child-component */}
-        {receivedOffers && sentOffers ? (
-          <UserOffersTable isSender={false} data={receivedOffers} handleOffer={processOffer} />
-        ) : null}
-        <hr />
-        <h2 className="section-title">Outgoing Offers</h2>
-        {/* Must wait for transactionsData before rendering component
+          {receivedOffers && sentOffers ? (
+            <UserOffersTable isSender={false} data={receivedOffers} handleOffer={processOffer} />
+          ) : null}
+          <hr />
+          <h2 className="section-title">Outgoing Offers</h2>
+          {/* Must wait for transactionsData before rendering component
         source: https://stackoverflow.com/questions/74656680/react-how-to-wait-on-prop-data-before-rendering-child-component */}
-        {receivedOffers && sentOffers ? (
-          <UserOffersTable isSender={true} data={sentOffers} handleOffer={processOffer} />
-        ) : null}
+          {receivedOffers && sentOffers ? (
+            <UserOffersTable isSender={true} data={sentOffers} handleOffer={processOffer} />
+          ) : null}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
